9212f7dfe1ad04f3a938b5b7272ade66656bea30
NicoRiot <nriot@umoove-inc.com>
2021-05-06 11:41:24 +0200

    dto

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,11 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Post } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { title } from 'process';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { Task } from './tasks.model';
 import { TasksService } from './tasks.service';
-
-
 
 @Controller('tasks')
 export class TasksController {
-    constructor(
-        private readonly taskservice : TasksService
-    ) {}
+    constructor(private readonly taskservice: TasksService) {}
+
+    @Get()
+    all(): Task[] {
+        return this.taskservice.getAll();
+    }
+
+    @Post()
+    create(@Body() createTaskDto : CreateTaskDto) {
+        return this.taskservice.createTask(createTaskDto);
+    }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './tasks.model';
+import * as uuid from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+    private tasks: Task[] = [];
+
+    getAll(): Task[] {
+        return this.tasks;
+    }
+
+    createTask(task : CreateTaskDto):Task {
+        const newTask: Task = {
+            id: uuid.v4(),
+            ...task,
+            status: TaskStatus.OPEN,
+        };
+
+        this.tasks.push(newTask);
+        return newTask;
+    }
+}
