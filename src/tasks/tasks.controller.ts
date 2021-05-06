eacee27cb3eb67c16ae8223aef54744f171065c0
NicoRiot <nriot@umoove-inc.com>
2021-05-06 12:24:10 +0200

    crud

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,8 +1,8 @@
-import { Body, Post } from '@nestjs/common';
+import { Delete, Patch } from '@nestjs/common';
+import { Body, Param, Post } from '@nestjs/common';
 import { Controller, Get } from '@nestjs/common';
-import { title } from 'process';
 import { CreateTaskDto } from './dto/create-task.dto';
-import { Task } from './tasks.model';
+import { Task, TaskStatus } from './tasks.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -18,4 +18,20 @@ export class TasksController {
     create(@Body() createTaskDto : CreateTaskDto) {
         return this.taskservice.createTask(createTaskDto);
     }
+
+    @Get(':id')
+    oneById(@Param('id') id : string) : Task {
+        return this.taskservice.one(id)
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id : string){
+        return this.taskservice.delete(id)
+    }
+
+    @Patch(':id/status')
+    update(@Param('id') id : string, @Body() updateTask : TaskStatus) : Task{
+        return this.taskservice.update(id, updateTask)
+    }
+
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@nestjs/common';
-import { Task, TaskStatus } from './tasks.model';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import * as uuid from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { Task, TaskStatus } from './tasks.model';
 
 @Injectable()
 export class TasksService {
@@ -11,14 +11,39 @@ export class TasksService {
         return this.tasks;
     }
 
-    createTask(task : CreateTaskDto):Task {
+    createTask(task: CreateTaskDto): Task {
         const newTask: Task = {
-            id: uuid.v4(),
+            id: uuid.v1(),
             ...task,
             status: TaskStatus.OPEN,
         };
 
         this.tasks.push(newTask);
         return newTask;
+    }
+
+    one(id: string): Task {
+        const task = this.tasks.find((task) => task.id === id);
+
+        if (!task) {
+            throw new NotFoundException('task not found');
+        }
+        return task;
+    }
+
+    delete(id: string): void {
+        const task = this.tasks.find((task) => task.id === id);
+        if (!task) {
+            throw new NotFoundException('task not found');
+        }
+
+        this.tasks = this.tasks.filter((task) => task.id !== id);
+    }
+
+    update(id: string, updateTasksDto: TaskStatus) {
+        const task = this.one(id);
+        task.status = updateTasksDto;
+
+        return task;
     }
 }
